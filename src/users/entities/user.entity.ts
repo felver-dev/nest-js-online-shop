c44005d7b4c9a7e0ace3d840f475b2c785168de0
felver-dev <felver.ngouala@gmail.com>
2024-12-18 12:50:04 +0100

    Mapping user entity to database table

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,29 @@
+import { RegistryDates } from 'common/embedded/registry-dates.embedded';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column({ unique: true })
   email: string;
+
+  @Column({ unique: true })
   phone: string;
+
+  @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
