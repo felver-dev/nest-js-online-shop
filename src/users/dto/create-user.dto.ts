ae59beb8e57c777abdddcaefc280a79d01b445bc
felver-dev <felver.ngouala@gmail.com>
2024-12-18 08:30:17 +0100

    Applying validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsPhoneNumber, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(2, 255)
   readonly name: string;
+
+  @IsString()
+  @IsEmail()
   readonly email: string;
+
+  @IsString()
+  @IsPhoneNumber('CI')
   readonly phone: string;
+
+  @IsString()
   readonly password: string;
 }
